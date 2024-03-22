fc5731c57a60f36e4f2fce67fe20e64df3dc64fa
Igor <igorgras100@gmail.com>
2024-03-22 22:41:15 +0000

    basic window opening with GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,36 @@
+#include <GLFW/glfw3.h>
 #include <iostream>
 
+void error_callback(int error, const char* description) {
+		fprintf(stderr, "Error: %s\n", description);
+}
+
 int main(){
-	std::cout << "Hello World!" << "\n";
+	if (!glfwInit()) {
+		return 1;
+	}
+	
+	glfwSetErrorCallback(error_callback);
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(640, 480, "TV", NULL, NULL);
+	if (!window) {
+		return 1;
+	}
+
+	glfwMakeContextCurrent(window);
+	
+	while (!glfwWindowShouldClose(window)) {
+		// keep running
+		
+	}
+	if (glfwWindowShouldClose(window)) {
+		glfwDestroyWindow(window);
+	}
+		
+	
+	glfwTerminate();
 	return 0;
 }
