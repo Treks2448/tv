07b161191d499965abcbe64cf7334fabadce1091
Igor <igorgras100@gmail.com>
2024-03-27 22:26:20 +0000

    clang-format run on main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,34 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-void error_callback(int error, const char* description) {
-		fprintf(stderr, "Error: %s\n", description);
+void error_callback(int error, const char *description) {
+  fprintf(stderr, "Error: %s\n", description);
 }
 
-int main(){
-	if (!glfwInit()) {
-		return 1;
-	}
-	
-	glfwSetErrorCallback(error_callback);
+int main() {
+  if (!glfwInit()) {
+    return 1;
+  }
 
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);	
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);	
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow* window = glfwCreateWindow(640, 480, "TV", NULL, NULL);
-	if (!window) {
-		return 1;
-	}
+  glfwSetErrorCallback(error_callback);
 
-	glfwMakeContextCurrent(window);
-	
-	while (!glfwWindowShouldClose(window)) {
-		// keep running
-		
-	}
-	if (glfwWindowShouldClose(window)) {
-		glfwDestroyWindow(window);
-	}
-		
-	
-	glfwTerminate();
-	return 0;
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  GLFWwindow *window = glfwCreateWindow(640, 480, "TV", NULL, NULL);
+  if (!window) {
+    return 1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    // keep running
+  }
+  if (glfwWindowShouldClose(window)) {
+    glfwDestroyWindow(window);
+  }
+
+  glfwTerminate();
+  return 0;
 }
